cda6cb404efac21669d23135895f1750b694e960
charles <charlescharlie1671998@gmail.com>
2025-09-21 09:24:38 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+window.addEventListener('scroll', function() {
+    var header = document.getElementById('header');
+  
+    if (window.scrollY > 25) {
+      if (header) {
+        header.classList.add('fixed-top');
+      }
+    } else {
+      if (header) {
+        header.classList.remove('fixed-top');
+      }
+    }
+  });
+
+
+
+//   
+
+document.addEventListener("DOMContentLoaded", function () {
+    var mobileBtn = document.querySelector(".menu_icon");
+    var sideMenu = document.querySelector(".side-menu");
+    var sideMenuCloseBtn = document.querySelector(".side-menu-close-btn img");
+
+    // Check if elements exist before adding event listeners
+    if (mobileBtn && sideMenu) {
+        mobileBtn.addEventListener("click", () => {
+            sideMenu.classList.add("active");
+            // document.body.style.position = "fixed";
+            // document.body.style.width = "100%";
+        });
+    }
+
+    if (sideMenuCloseBtn) {
+        sideMenuCloseBtn.addEventListener("click", () => {
+            sideMenu.classList.remove("active");
+        });
+    }
+
+    document.addEventListener("click", (event) => {
+        if (sideMenu && mobileBtn && !sideMenu.contains(event.target) && !mobileBtn.contains(event.target)) {
+            sideMenu.classList.remove("active");
+            // document.body.style.position = "relative";
+        }
+    });
+
+    document.querySelectorAll(".mobile-menu > ul > li > a").forEach((item) => {
+        item.addEventListener("click", function (event) {
+            const submenu = this.nextElementSibling;
+            if (submenu && submenu.classList.contains("submenu")) {
+                const isOpen = submenu.style.display === "block";
+
+                if (!isOpen) {
+                    event.preventDefault();
+                    document.querySelectorAll(".mobile-menu .submenu").forEach((sub) => {
+                        sub.style.display = "none";
+                    });
+                    submenu.style.display = "block";
+                }
+            }
+        });
+    });
+});
+
+  let percent = { value: 0 };
+
+    // Animate battery fill and percentage
+    gsap.to(percent, {
+      value: 100,
+      duration: 1.5, // total loading time
+      ease: "linear",
+      onUpdate: () => {
+        let val = Math.round(percent.value);
+        document.querySelector(".charge").style.width = val + "%";
+        document.querySelector(".loading-text").textContent = `Charging... ${val}%`;
+      },
+      onComplete: () => {
+        gsap.to(".loader", {
+          opacity: 0,
+          duration: 0.8,
+          onComplete: () => {
+            document.querySelector(".loader").style.display = "none";
+            document.querySelector("div:nth-of-type(2)").style.display = "block";
+          }
+        });
+      }
+    });
